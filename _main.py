435aadf2bebf05bdfa250bc4edd7aef2ef4fc61c
TheOneTrueCec <cec@conklinsystems.com>
2024-12-19 14:36:55 -0500

    Update _main.py

---
+++ _main.py
@@ -0,0 +1,31 @@
+import os
+import configparser
+
+import discord
+from discord.ext import tasks
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+client.run(TOKEN)
+
+@tasks.loop(hours=1)
+async def refreshDatabase():
+    return
